917d4a62f25bb6027313e95486aa5b3bfff6cd89
Weinan Li <l.weinan@gmail.com>
2018-05-25 02:22:11 +0800

    vector_demo.cc

--- vector_demo.cc
+++ vector_demo.cc
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main() {
-    std::vector<int> ivec;
+    vector<int> ivec;
     ivec.push_back(42);
     ivec.push_back(24);
-    std::cout << ivec[0] << std::endl;
-    std::cout << ivec[1] << std::endl;
+    cout << ivec[0] << endl;
+    cout << ivec[1] << endl;
+
+    string word;
+    vector<string> text;
+
+    cout << "Input your text: ";
+    while(cin >> word && word.compare(".") != 0) {
+        text.push_back(word);
+    }
+
+    for (auto iter = text.cbegin(); iter != text.cend() && !iter->empty(); ++iter) {
+        cout << *iter << endl;
+    }
+
+    return 0;
 }
