f24319647d1f3af82839b3abb5dbd5943c1c730c
Weinan Li <l.weinan@gmail.com>
2018-05-28 18:24:46 +0800

    .

--- scores.cc
+++ scores.cc
@@ -1,7 +1,18 @@
 #include <vector>
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    
+    vector<unsigned> scores(11, 0);
+    unsigned grade, i = 0;
+    while (cin >> grade && i < scores.size()) {
+        ++scores[grade / 10];
+        i++;
+    }
+
+    for (auto iter = scores.begin(); iter != scores.end(); iter++) {
+        cout << *iter << endl;
+    }
     return 0;
 }
