d34d8cf86fdf903c7194809c62e9d2806f2db3e5
Weinan Li <l.weinan@gmail.com>
2018-06-10 18:28:15 +0800

    .

---
+++ make_shared.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <memory>
+
+void foo(const std::shared_ptr<int> &i) {
+    (*i)++;
+}
+
+int main() {
+    auto sp = std::make_shared<int>(12);
+    foo(sp);
+    std::cout << *sp << '\n';
+}
