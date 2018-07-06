a2fc58e8b3456789934d1eb6e73420d458699eb5
Weinan Li <l.weinan@gmail.com>
2018-07-07 01:11:51 +0800

    .

--- file_io.c
+++ file_io.c
@@ -10,4 +10,12 @@ int main(void) {
     assert(f1);
     size_t r1 = fwrite("CAFE\n", sizeof(char), sizeof("CAFE\n"), f1);
     fclose(f1);
+
+    char a[4] = {'C', 'A', 'F', 'E'};
+    printf("sizeof a: %d\n", sizeof(a));
+
+    FILE *f2 = fopen("file2.bin", "wb");
+    assert(f2);
+    fwrite(a, sizeof(char), sizeof(a), f2);
+    fclose(f2);
 }
