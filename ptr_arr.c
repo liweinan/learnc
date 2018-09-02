f21cef3f6c4f057e3c15b132653eaaedcfc03893
Weinan Li <l.weinan@gmail.com>
2018-09-02 12:44:08 +0800

    .

---
+++ ptr_arr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *names[] = {"Miller", "Jones", "Anderson"};
+
+    printf("%c\n", *(*(names + 1) + 2));
+    printf("%c\n", names[1][2]);
+
+
+    printf("%p\n", (*(names + 1) + 2));
+    printf("%p\n", &names[1][2]);
+}
