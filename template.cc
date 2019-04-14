a22bd62fa6ccbda1c92700a383ef50d1e9823572
Weinan Li <l.weinan@gmail.com>
2019-04-14 09:31:22 +0800

    .

--- template.cc
+++ template.cc
@@ -1,12 +1,36 @@
 #include <iostream>
 
+// http://www.cplusplus.com/doc/oldtutorial/templates/
 template<class T>
 T getMax(T a, T b) {
     return (a > b ? a : b);
 }
 
+template<class U>
+class Pair {
+    U x;
+    U y;
+public:
+    U getX() const {
+        return x;
+    }
+
+    U getY() const {
+        return y;
+    }
+
+    Pair(U x, U y) {
+        this->x = x;
+        this->y = y;
+        std::cout << this->x << ", " << this->y << std::endl;
+    }
+};
+
+
 int main() {
     int x = 3, y = 10;
 
     std::cout << getMax<int>(x, y) << std::endl;
+
+    Pair<int> dot(2, 3);
 }
