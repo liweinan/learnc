ea5f0afbe6cf386e7cb58e621241b14b1d8bcd20
Weinan Li <l.weinan@gmail.com>
2019-05-13 10:20:47 +0800

    threads

---
+++ thread_args.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+void threadCallback(int x, std::string str) {
+    std::cout << "Passed Number = " << x << std::endl;
+    std::cout << "Passed String = " << str << std::endl;
+}
+
+int main() {
+    int x = 10;
+    std::string str = "Sample String";
+    std::thread threadObj(threadCallback, x, str);
+    threadObj.join();
+    return 0;
+}
