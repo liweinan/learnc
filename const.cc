9266f184ce70d93bce3a4d869a2e18d660dca844
weli <l.weinan@gmail.com>
2019-05-19 22:01:34 +0800

    .

--- const.cc
+++ const.cc
@@ -8,5 +8,15 @@ int main() {
     int y = 24;
     const int *p = &x; // pointer to a const int variable
     int * const q = &y; // q is a const pointer
-    q = &x; // compile error
+//    q = &x; // compile error
+
+    const int z = 14;
+    int o = z;
+//    int *m = &z; // compile error
+    const int *m = &z; // ok
+
+//    int * const n = &z; // compile error
+
+    int const* const n = &z;
+    const int* const n2 = &z;
 }
